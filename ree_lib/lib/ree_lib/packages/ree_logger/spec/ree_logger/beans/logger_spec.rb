1e6914e5cc1d52703b6b0cad6272b514db30c2eb
Konstantin Kim <37348392+neonaddict@users.noreply.github.com>
2025-07-30 16:47:22 +0300

    Revert "add sentry appender"

--- ree_lib/lib/ree_lib/packages/ree_logger/spec/ree_logger/beans/logger_spec.rb
+++ ree_lib/lib/ree_lib/packages/ree_logger/spec/ree_logger/beans/logger_spec.rb
@@ -16,7 +16,6 @@ RSpec.describe :logger do
 
   before(:each) do
     allow(Rollbar).to receive(:log)
-    allow(Sentry).to receive(:capture_message)
   end
 
   let(:log_file_path) { ENV['LOG_FILE_PATH'] }
@@ -28,28 +27,24 @@ RSpec.describe :logger do
   it {
     expect { logger.info('hello world') }.to output(/hello world/).to_stdout
     expect(Rollbar).to have_received(:log)
-    expect(Sentry).to have_received(:capture_message)
     expect(File.read(log_file_path)).to match("hello world")
   }
 
   it {
     expect { logger.info('hello world', { rollbar_scope: {fingerprint: 'test', test: 'test'}, param: 1, another_param: { name: 'John'} }) }.to output(/John/).to_stdout
     expect(Rollbar).to have_received(:log)
-    expect(Sentry).to have_received(:capture_message)
     expect(File.read(log_file_path)).to match("John")
   }
 
   it {
     expect { logger.debug('debug message') }.to_not output(/debug message/).to_stdout
     expect(Rollbar).not_to have_received(:log)
-    expect(Sentry).not_to have_received(:capture_message)
     expect(File.read(log_file_path)).to_not match("debug")
   }
 
   it {
     expect { logger.warn('warning message') }.to output(/warning message/).to_stdout
     expect(Rollbar).to have_received(:log)
-    expect(Sentry).to have_received(:capture_message)
     expect(File.read(log_file_path)).to match("warning message")
   }
 
@@ -60,7 +55,6 @@ RSpec.describe :logger do
     expect(output).to match(/some error message/)
     expect(output).to_not match(/method|args/)
     expect(Rollbar).to have_received(:log)
-    expect(Sentry).to have_received(:capture_message)
     expect(File.read(log_file_path)).to match("some error message")
     expect(File.read(log_file_path)).to_not match("PARAMETERS: {:method=>{:name=>:call, :args=>{:block=>{}}}}")
   }
@@ -68,14 +62,12 @@ RSpec.describe :logger do
   it {
     expect { logger.fatal('some fatal message', {}, exception) }.to output(/some fatal message/).to_stdout
     expect(Rollbar).to have_received(:log)
-    expect(Sentry).to have_received(:capture_message)
     expect(File.read(log_file_path)).to match("some fatal message")
   }
 
   it {
     expect { logger.unknown('unknown message') }.to output(/unknown message/).to_stdout
     expect(Rollbar).to have_received(:log)
-    expect(Sentry).to have_received(:capture_message)
     expect(File.read(log_file_path)).to match("unknown message")
   }
 end
